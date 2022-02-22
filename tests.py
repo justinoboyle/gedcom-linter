addfa92ccbbdf22c43defdbee4124769c5a7d048
Justin O'Boyle <justin@justinoboyle.com>
2022-02-21 21:56:31 -0500

    Add deliverable

---
+++ tests.py
@@ -0,0 +1,22 @@
+import unittest
+from src import * # import our project's source code
+
+# Data is piped in from run-tests shell script to stdin
+
+# run it on the input 
+lines = runLinter()
+individuals, families = runParser(lines)
+
+# run tests
+class TestGEDCOM(unittest.TestCase):
+
+    # US22 - Unique IDs
+    # All individual IDs should be unique and all family IDs should be unique
+    def test_US22(self):
+        ''' Check if given list contains any duplicates '''
+        return self.assertEqual(len(individuals), len(set(individuals))) and self.assertEqual(len(families), len(set(families)))
+
+
+# required unittest boilerplate
+if __name__ == '__main__':
+    unittest.main()
