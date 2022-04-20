6b8496760ea105af191dad64f1cb2eced16f3505
Justin O'Boyle <justin@justinoboyle.com>
2022-04-19 23:44:57 -0400

    Large refactor to fix tests (#64)

--- src/__init__.py
+++ src/__init__.py
@@ -51,6 +51,13 @@ class ParsedGEDCOM:
     def __str__(self):
         return str(self.level) + "|" + self.tag + "|" + ("Y" if self.valid else "N") + "|" + " ".join(self.args)
 
+errorBuffer = []
+failMode = False
+def doError(error):
+    if failMode:
+        raise Exception(error)
+    errorBuffer.append(error)
+
 def runLinter():
     # Read from standard input and return an array of ParsedGEDCOM objects
     lines = []
@@ -59,9 +66,25 @@ def runLinter():
         lines.append(ParsedGEDCOM(line))
     return lines
 
+idBuffer = []
+
+# Use this buffer for things that have to be unique -- e.g. IDs
+def uniqueIdBuffer(newID, idType):
+    global idBuffer
+    if (idType + newID) in idBuffer:
+        # TODO Add a feature to make this error message better.
+        doError("ID " + newID + " is not unique!")
+    else:
+        idBuffer.append(idType + newID)
+
+def flushIDBuffer():
+    global idBuffer
+    idBuffer = []
+
 class Individual:
     def __init__(self, id, level):
         self.id = id
+        uniqueIdBuffer(id, "INDI")
         self.level = level
         self.name = "N/A"
         self.sex = "N/A"
@@ -73,20 +96,28 @@ class Individual:
         self.spouse = "N/A"
         self.family = "N/A"
 
+    def setSpouse(self, spouse):
+        if(self.spouse != "N/A"):
+            doError("Bigamy: already married! (Individual ID " + self.id + ")")
+        self.spouse = spouse
+
     def setAge(self, age):
         if age >= 150:
-            raise Exception("Age must be less than 130!")
+            doError("Age must be less than 150! (Individual ID " + self.id + ")")
         self.age = age
 
     def validateBirthDeath(self):
+        if datetime.datetime.strptime(self.birthday, '%Y-%m-%d') > datetime.datetime.now():
+            doError("Birthday must be before current date! (Individual ID " + self.id + ")")
         if self.birthday == "N/A" or self.death == "N/A":
             pass
         else:
             if dateFromString(self.birthday) > dateFromString(self.death):
-                raise Exception("Birthday must be before death!")
+                doError("Birthday must be before death! (Individual ID " + self.id + ")")
 
     def setBirthday(self, birthday):
         self.birthday = birthday
+        uniqueIdBuffer(self.name + self.birthday, "INDI-BDAYD")
         self.validateBirthDeath()
 
     def setDeath(self, death):
@@ -99,7 +130,7 @@ class Individual:
         '''verify that birth comes before marriage'''
         marriage = self.family.married
         if datetime.datetime.strptime(self.birthday, '%Y-%m-%d') < datetime.datetime.strptime(marriage, '%Y-%m-%d'):
-            raise Exception("Birthday must be before marriage!")
+            doError("Birthday must be before marriage! (Individual ID " + self.id + ")")
 
     # US06 (Irakli)
     # Divorce can only occur before death of both spouses
@@ -107,7 +138,7 @@ class Individual:
         '''verify that divorce comes before death'''
         divorce = self.family.divorced
         if datetime.datetime.strptime(self.death, '%Y-%m-%d') < datetime.datetime.strptime(divorce, '%Y-%m-%d'):
-            raise Exception("Divorce must be before death!")
+            doError("Divorce must be before death! (Individual ID " + self.id + ")")
 
 
     def __str__(self):
@@ -119,6 +150,7 @@ class Individual:
 class Family:
     def __init__(self, id):
         self.id = id
+        uniqueIdBuffer(id, "INDI")
         self.married = "N/A"
         self.divorced = "N/A"
         self.husbandId = "N/A"
@@ -126,16 +158,27 @@ class Family:
         self.wifeId = "N/A"
         self.wifeName = "N/A"
         self.children = []
+        self._wifeRef = None
+        self._husbRef = None
+
 
     def addHusband(self, husband):
         if husband.family != "N/A":
-            raise Exception("Husband already has a family!")
+            doError("Husband " + husband.id + " is already in a family and cannot be added to " + self.id + "!")
         self.husbandId = husband.id
         self.husbandName = husband.name
+        self._husbRef = husband
+
+    def addWife(self, wife):
+        if wife.family != "N/A":
+            doError("Wife " + wife.id + " is already in a family and cannot be added to " + self.id + "!")
+        self.husbandId = wife.id
+        self.husbandName = wife.name
+        self._wifeRef = wife
 
     def addChild(self, child):
         if dateFromString(self.married) > dateFromString(child.birthday):
-            raise Exception("Child can't be born before parents were married!")
+            doError("Child can't be born before parents were married!")
             
         # get last name of husbandName
         husbandLastName = self.husbandName.split(" ")[-1]
@@ -145,27 +188,30 @@ class Family:
         if husbandLastName == childLastName:
             self.children.append(child.id)
         else:
-            raise Exception("Last name of husband and child must match!")
-
-    # US11 No Bigamy (Irakli)
-    # Marriage should not occur during marriage to another spouse
-    def isBigamy(families):
-        indis = {}
-        for fam in families:
-            if fam.wifeId in indis or fam.husbandId in indis:
-                raise Exception("Bigamy found in family id " + fam.id)
-            else:
-                indis[fam.wifeId] = 1
-                indis[fam.husbandId] = 1
-        return False
+            doError("Last name of husband and child must match!")
         
     # US15 Fewer than 15 siblings (Irakli)
     # There should be fewer than 15 siblings in a family
     def tooManySiblings(families):
         for fam in families:
             if len(fam.children) >= 15:
-                raise Exception("Too many siblings in family id " + fam.id)
+                doError("Too many siblings in family id " + fam.id)
         return True
+
+    def setMarried(self, married):
+        self.married = married
+        self.validateMarriedDivorced()
+    
+    def setDivorced(self, divorced):
+        self.divorced = divorced
+        self.validateMarriedDivorced()
+
+    def validateMarriedDivorced(self):
+        if self.married == "N/A" or self.divorced == "N/A":
+            pass
+        else:
+            if dateFromString(self.married) > dateFromString(self.divorced):
+                 doError("FAMILY: US04: " + self.id + ": Marriage occurs after divorce")
 
     def __str__(self):
         return self.id
@@ -201,7 +247,12 @@ def toMonths(str):
     else:
         return "N/A"
 
-def runParser(lines):
+def setFailMode(mode):
+    global failMode
+    failMode = mode
+
+def runParser(lines, isFailMode):
+    setFailMode(isFailMode)
     # Return a list of individuals and a list of families based on tags
     families = []
     # Initialize all families so that individuals can be inserted
@@ -242,11 +293,11 @@ def runParser(lines):
                     for fam in families:
                         if fam.id == line.args[0]:
                             if individuals[-1].sex == "M":
-                                individuals[-1].spouse = fam
+                                individuals[-1].setSpouse(fam)
                                 fam.husbandId = individuals[-1].id
                                 fam.husbandName = individuals[-1].name
                             else:
-                                individuals[-1].spouse = fam
+                                individuals[-1].setSpouse(fam)
                                 fam.wifeId = individuals[-1].id
                                 fam.wifeName = individuals[-1].name
                 elif line.level != 0:
@@ -262,10 +313,10 @@ def runParser(lines):
                 line = lines[i]
                 if line.tag == "MARR":
                     i+=1
-                    families[-1].married = lines[i].args[0] + " " + lines[i].args[1] + " " + lines[i].args[2]
+                    families[-1].setMarried(lines[i].args[0] + " " + lines[i].args[1] + " " + lines[i].args[2])
                 elif line.tag == "DIV":
                     i+=1
-                    families[-1].divorced = lines[i].args[0] + " " + lines[i].args[1] + " " + lines[i].args[2]
+                    families[-1].setDivorced(lines[i].args[0] + " " + lines[i].args[1] + " " + lines[i].args[2])
                 elif line.tag == "HUSB":
                     families[-1].husband_id = line.args[0]
                     for indi in individuals:
@@ -300,9 +351,9 @@ def runParser(lines):
                         mergedFam.wife_id = fam.wife_id
                         mergedFam.wife_name = fam.wife_name
                     if fam.married != "N/A":
-                        mergedFam.married = fam.married
+                        mergedFam.setMarried(fam.married)
                     if fam.divorced != "N/A":
-                        mergedFam.divorced = fam.divorced
+                        mergedFam.setDivorced(fam.divorced)
                     if fam.children != []:
                         mergedFam.children.extend(fam.children)
     
@@ -314,13 +365,6 @@ def runParser(lines):
             indi.setAge((datetime.datetime.today() - datetime.datetime.strptime(indi.birthday, "%Y-%m-%d")).days // 365)
         else:
             indi.setAge((datetime.datetime.strptime(indi.death, "%Y-%m-%d") - datetime.datetime.strptime(indi.birthday, "%Y-%m-%d")).days // 365)
-
-    # US04 Marriage before divorce (Max)
-    # for each family, make sure that the date of marriage is before the date of divorce
-    for fam in mergedFamilies:
-        if fam.married != "N/A" and fam.divorced != "N/A":
-            if datetime.datetime.strptime(fam.married, "%Y-%m-%d") > datetime.datetime.strptime(fam.divorced, "%Y-%m-%d"):
-                print("ERROR: FAMILY: US04: " + fam.id + ": Marriage occurs after divorce")
 
     # US09 Birth before death of parents
     # for each family make sure every child has a birthday before the death of the mother and father
@@ -337,16 +381,16 @@ def runParser(lines):
                                 motherDeath = indi2.death
                         # if the child's birthday is before the death of the father or mother, print an error
                         if (fatherDeath != "N/A") and datetime.datetime.strptime(indi.birthday, "%Y-%m-%d") > datetime.datetime.strptime(fatherDeath, "%Y-%m-%d"):
-                            print("ERROR: FAMILY: US09: " + fam.id + ": Child " + indi.id + " born " + indi.birthday + " after death of father " + fam.husband_id + " " + fatherDeath)
+                            doError("FAMILY: US09: " + fam.id + ": Child " + indi.id + " born " + indi.birthday + " after death of father " + fam.husband_id + " " + fatherDeath)
                         if (motherDeath != "N/A") and datetime.datetime.strptime(indi.birthday, "%Y-%m-%d") > datetime.datetime.strptime(motherDeath, "%Y-%m-%d"):
-                            print("ERROR: FAMILY: US09: " + fam.id + ": Child " + indi.id + " born " + indi.birthday + " after death of mother " + fam.wife_id + " " + motherDeath)
+                            doError("FAMILY: US09: " + fam.id + ": Child " + indi.id + " born " + indi.birthday + " after death of mother " + fam.wife_id + " " + motherDeath)
     
     # US11 No Bigamy (Irakli)
     # Marriage should not occur during marriage to another spouse
     indis = {}
     for fam in mergedFamilies:
         if fam.wifeId in indis or fam.husbandId in indis:
-            print('bigamy')
+            doError("Bigamy found in family id " + fam.id)
         else :
             indis[fam.wifeId] = 1
             indis[fam.husbandId] = 1
@@ -370,7 +414,7 @@ def runParser(lines):
                         # if the birthdays of the siblings greater than 2 days but less than 8 months apart, print an error
                         for sibling in siblings:
                             if (datetime.datetime.strptime(childBirth, "%Y-%m-%d") - datetime.datetime.strptime(sibling, "%Y-%m-%d")).days > 2 and (datetime.datetime.strptime(childBirth, "%Y-%m-%d") - datetime.datetime.strptime(sibling, "%Y-%m-%d")).days < 8*30:
-                                print("ERROR: FAMILY: US13: " + fam.id + ": Sibling " + sibling + " born " + sibling + " more than 2 days apart from child " + child + " born " + childBirth)
+                                doError("FAMILY: US13: " + fam.id + ": Sibling " + sibling + " born " + sibling + " more than 2 days apart from child " + child + " born " + childBirth)
     
     # US18 Siblings should not marry one another
     # for each family, make sure that the marriage of siblings should not occur
@@ -391,25 +435,27 @@ def runParser(lines):
                         # if the birthdays of the siblings are the same, print an error
                         for sibling in siblings:
                             if sibling == childBirth:
-                                print("ERROR: FAMILY: US18: " + fam.id + ": Sibling " + sibling + " born " + sibling + " married to child " + child + " born " + childBirth)
+                                doError("FAMILY: US18: " + fam.id + ": Sibling " + sibling + " born " + sibling + " married to child " + child + " born " + childBirth)
     
     # US23 Unique name and birth date (Max) 
     # for each individual, make sure that everyone has a unique name
     for indi in individuals:
         for indi2 in individuals:
             if indi.name == indi2.name and indi.id != indi2.id:
-                print("ERROR: INDIVIDUAL: US23: " + indi.id + ": " + indi.name + ": is the same name as " + indi2.id + ": " + indi2.name)
+                doError("INDIVIDUAL: US23: " + indi.id + ": " + indi.name + ": is the same name as " + indi2.id + ": " + indi2.name)
 
+    return postvalidate(individuals, mergedFamilies)
+def postvalidate(individuals, mergedFamilies):
     # US24 Unique families by spouses
     # No more than one family with the same spouses by name and the same marriage date should appear in a GEDCOM file
     for fam in mergedFamilies:
       # US15 Fewer than 15 siblings (Irakli)
       # There should be fewere than 15 siblings in a family
         if len(fam.children) >= 15:
-            print("too many siblings")
+            doError("Too many siblings in family id " + fam.id)
         for fam2 in mergedFamilies:
             if fam.married == fam2.married and fam.husband_id == fam2.husband_id and fam.wife_id == fam2.wife_id and fam.id != fam2.id:
-                print("ERROR: FAMILY: US24: " + fam.id + ": " + fam.married + ": " + fam.husband_id + " and " + fam.wife_id + ": are the same family")
+                doError("FAMILY: US24: " + fam.id + ": " + fam.married + ": " + fam.husband_id + " and " + fam.wife_id + ": are the same family")
 
 
     return individuals, mergedFamilies
@@ -423,7 +469,7 @@ def checkConsistency(individual, family):
     for child in family.children:
         if indiID == child.id:
             return True
-    raise Exception("individual info is inconsistent with family info!")
+    doError("individual info is inconsistent with family info!")
 
 def printer(individuals, families):
     # create a table of individuals and families using ljust to create whitespace and keep table aligned
